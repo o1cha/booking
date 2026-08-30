106c9f9b1571bcdf85a5b23f1e90c70f14d13bbe
Olcha <o1cha@mail.ru>
2026-08-30 23:33:07 +0300

    feat(hotels) add Room model

--- hotels/models.py
+++ hotels/models.py
@@ -16,3 +16,16 @@ class Hotel(models.Model):
         ],
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Room(models.Model):
+    hotel = models.ForeignKey(
+        Hotel,
+        on_delete=models.CASCADE,
+        related_name="rooms",
+    )
+    number = models.PositiveIntegerField()
+    room_type = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    capacity = models.PositiveIntegerField()
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
