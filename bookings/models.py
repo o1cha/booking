3a13f21287fd07d388ff8d8ce8dfd8bb8deffe3a
Olcha <o1cha@mail.ru>
2026-08-31 00:14:50 +0300

    feat(bookings): add Booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from config import settings
 
-# Create your models here.
+
+class Booking(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="bookings",
+    )
+    room = models.ForeignKey(
+        "hotels.Room",
+        on_delete=models.CASCADE,
+        related_name="bookings"
+    )
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    status = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
