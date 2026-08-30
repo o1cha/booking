d5543c104c9805c8c47447ee6215d0cf21287451
Olcha <o1cha@mail.ru>
2026-08-30 20:47:08 +0300

    feat(hotels): add Hotel model

--- hotels/models.py
+++ hotels/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    rating = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(5),
+        ],
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
